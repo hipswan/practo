6b55d820cca59ee9177c6f91e9f97b85a02e96e9
Atul Singh <46633779+hipswan@users.noreply.github.com>
2020-05-03 22:36:51 +0530

    trekking apps

---
+++ app.js
@@ -0,0 +1,21 @@
+new fullpage('#fullpage', {
+	//options here
+	autoScrolling:true,
+    navigation:true,
+    onLeave: function(origin, destination, direction){
+        const section = destination.item;
+        const title = section.querySelector('h1');
+        const parasec = section.querySelectorAll('p');
+        const t1= new TimelineMax({delay:0.4});
+        t1.fromTo(title,0.5,{y:'50',opacity:0},{y:'0',opacity:1})
+        .fromTo(parasec,0.5,{x:'-50',opacity:0},{x:'0',opacity:1});
+        if(destination.index === 1){
+            const img_mountain = document.querySelectorAll('.type-mountain');
+            console.log(img_mountain);
+            t1.fromTo(img_mountain, 0.7, {x:'100%',opacity:1},{x:'-35%',opacity:0})
+            .fromTo(img_mountain[0], 0.7, {opacity:0},{opacity:1});
+           
+        }
+
+    },
+});
